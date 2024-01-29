18b4aebf0633851ba69eac962c87ea66f252ef59
jher235 <tim668666@gmail.com>
2024-01-29 23:48:56 +0900

    userDao 메소드 예외처리

--- Toby_Spring_3.1/springbook/src/main/java/springbook/user/dao/UserDao.java
+++ Toby_Spring_3.1/springbook/src/main/java/springbook/user/dao/UserDao.java
@@ -86,30 +86,70 @@ public class UserDao {
 
 
     public void deleteAll() throws SQLException {
-        Connection c = dataSource.getConnection();
+        PreparedStatement ps = null;
+        Connection c = null;
 
-        PreparedStatement ps = c.prepareStatement("delete from users");
-        ps.executeUpdate();
+        try {
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("delete from users");
+            ps.executeUpdate();
+        }catch (SQLException e){
+            throw e;
+        }finally {
+            if(ps != null){ //null 일때 close하면 nullpointexception이 발생함
+                try {
+                    ps.close();
+                }catch(SQLException e){
+                }
+            }
+            if(c!=null){
+                try{
+                    c.close();
+                }catch (SQLException e){
+                }
+            }
+        }
 
-        ps.close();
-        c.close();
+
+
     }
 
 
     public int getCount() throws SQLException {
-        Connection c = dataSource.getConnection();
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
 
-        PreparedStatement ps = c.prepareStatement("select count(*) from users");
+        try {
+            c = dataSource.getConnection();
 
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        int count = rs.getInt(1);
+            ps = c.prepareStatement("select count(*) from users");
+            rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        }catch (SQLException e){
+            throw e;
+        }finally {      //close()는 만들어진 순서의 반대로 하는 것이 원칙이다.
+            if(rs!=null){
+                try{
+                    rs.close();
+                }catch (SQLException e){
+                }
+            }
+            if(ps!=null){
+                try {
+                    ps.close();
+                }catch (SQLException e){
+                }
+            }
+            if(c!=null){
+                try {
+                    c.close();
+                }catch (SQLException e){
+                }
+            }
 
-        rs.close();
-        c.close();
-        ps.close();
-
-        return count;
+        }
     }
 
 //    private static Connection getConnection() throws ClassNotFoundException, SQLException {
