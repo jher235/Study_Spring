ce1d24bb481c6429b7e356433d8c1b088d5e2483
jher235 <tim668666@gmail.com>
2024-01-31 01:57:31 +0900

    3장 마무리, jdbcTamplate 적용

---
+++ Toby_Spring_3.1/springbook/ch3/main/java/springbook/user/dao/DaoFactory.java
@@ -0,0 +1,43 @@
+package springbook.user.dao;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DaoFactory {
+
+    @Bean
+    public UserDao userDao(){
+        UserDao userDao = new UserDao();        //메소드를 통한 주입
+//        userDao.setConnectionMaker(connectionMaker());
+        userDao.setDataSource(dataSource());
+        return userDao;
+
+//        return new UserDao(connectionMaker());       //생성자를 통한 주입 시
+    }
+
+
+//    public AccountDao accountDao(){
+//        return new AccountDao(connectionMaker());
+//    }
+
+    @Bean
+    public ConnectionMaker connectionMaker() {
+        return new MySqlConnectionMaker();
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
+        dataSource.setUrl("jdbc:mysql://localhost/tobyspring3_1?");
+        dataSource.setUsername("root");
+        dataSource.setPassword("jher235");
+
+        return dataSource;
+    }
+}
---
+++ Toby_Spring_3.1/springbook/ch3/main/java/springbook/user/dao/MySqlConnectionMaker.java
@@ -0,0 +1,14 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySqlConnectionMaker implements ConnectionMaker{
+    public Connection makeConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/tobyspring3_1?serverTimezone=UTC","root","jher235");
+        return c;
+    }
+}
