48674d62c4c544392b783ede643c7f5f6650642c
jher235 <tim668666@gmail.com>
2024-01-31 18:39:48 +0900

    4장 마무리

---
+++ Toby_Spring_3.1/springbook/src/main/java/springbook/user/dao/UserDaoJdbc.java
@@ -0,0 +1,74 @@
+package springbook.user.dao;
+
+import org.springframework.dao.DuplicateKeyException;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import springbook.user.domain.User;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.List;
+
+public class UserDaoJdbc implements UserDao{
+
+    private DataSource dataSource;      //스프링에서 제공하는 인터페이스 객체
+
+    private JdbcContext jdbcContext;
+    private JdbcTemplate jdbcTemplate;
+
+    public void setDataSource(DataSource dataSource) {
+
+        this.jdbcTemplate = new JdbcTemplate(dataSource);   //jdbcTemplate 방식
+
+    }
+
+
+    public void add(final User user) throws DuplicateUserIdException {
+        this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getId(), user.getName(), user.getPassword());
+//        try {
+//            this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getId(), user.getName(), user.getPassword());
+//        }
+//        catch (DuplicateKeyException e){
+//            System.out.println(e.getMessage());
+//            throw new DuplicateUserIdException(e);
+//        }
+    }
+
+    public class DuplicateUserIdException extends RuntimeException{
+        public DuplicateUserIdException(Throwable cause){
+            super(cause);
+        }
+    }
+
+    public User get(String id){
+        return this.jdbcTemplate.queryForObject("select * from users where id = ?",new Object[]{id},this.userMapper);
+    }
+
+
+    public void deleteAll() {
+        this.jdbcTemplate.update("delete from users");  //JdbcTemplate의 내장 콜백을 사용함.
+    }
+
+
+    public int getCount()  {
+        return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
+    }
+
+    public List<User> getAll(){
+        return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
+    }
+
+    private RowMapper<User> userMapper =
+            new RowMapper<User>() {
+                @Override
+                public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                    User user = new User();
+                    user.setId(rs.getString("id"));
+                    user.setName(rs.getString("name"));
+                    user.setPassword(rs.getString("password"));
+                    return user;
+                }
+            };
+
+}
