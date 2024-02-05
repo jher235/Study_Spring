8408d206f262e8b5413985202f29f00e1fb108e3
jher235 <tim668666@gmail.com>
2024-02-06 00:12:07 +0900

    스프링의 ProxyFactoryBean

--- Toby_Spring_3.1/springbook/src/test/java/springbook/user/service/UserServiceTest.java
+++ Toby_Spring_3.1/springbook/src/test/java/springbook/user/service/UserServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.springframework.aop.framework.ProxyFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.mail.MailException;
@@ -227,7 +228,8 @@ class UserServiceTest {
 
         testUserService.setUserLevelUpgradePolicy(this.userLevelUpgradePolicy);
 
-        TxProxyFactoryBean txProxyFactoryBean = context.getBean("&userService", TxProxyFactoryBean.class);
+//        TxProxyFactoryBean txProxyFactoryBean = context.getBean("&userService", TxProxyFactoryBean.class);
+        ProxyFactoryBean txProxyFactoryBean = context.getBean("&userService", ProxyFactoryBean.class);
         txProxyFactoryBean.setTarget(testUserService);
         UserService txUserService = (UserService) txProxyFactoryBean.getObject();
 
