17b0ca9315b04e6f144bde2087f569dd86471259
jher235 <tim668666@gmail.com>
2024-02-19 23:25:21 +0900

    쿠키를  통해 최근 기록 저장

--- Java_Web_Workbook/w2/src/main/java/org/zerock/w2/controller/TodoReadController.java
+++ Java_Web_Workbook/w2/src/main/java/org/zerock/w2/controller/TodoReadController.java
@@ -2,6 +2,7 @@ package org.zerock.w2.controller;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -22,9 +23,32 @@ public class TodoReadController extends HttpServlet {
         try {
 //            Long tno = Long.valueOf(req.getParameter("tno"));
             Long tno = Long.parseLong(req.getParameter("tno"));
+
             TodoDTO todoDTO =  todoService.get(tno);
 
+            //모델 담기
             req.setAttribute("dto",todoDTO );
+
+            //쿠키 찾기
+            Cookie viewTodoCookie = findCookie(req.getCookies(), "viewTodos");
+            String todoListStr = viewTodoCookie.getValue();
+            boolean exist = false;
+
+            if(todoListStr != null && todoListStr.indexOf(tno+"-") >= 0){
+                exist = true;
+            }
+
+            log.info("exist : "+ exist);
+
+            if(!exist){
+                todoListStr += tno+"-";
+                viewTodoCookie.setValue(todoListStr);
+                viewTodoCookie.setPath("/");
+                viewTodoCookie.setMaxAge(60*60*24);
+                resp.addCookie(viewTodoCookie);
+            }
+
+
             req.getRequestDispatcher("/WEB-INF/todo/read.jsp").forward(req,resp);
 
 
@@ -32,5 +56,30 @@ public class TodoReadController extends HttpServlet {
             log.error(e.getMessage());
             throw new ServletException("read error");
         }
+    }
+
+    //쿠키 모음들 (Cookie[] cookies)로 부터 cookieName에 해당하는 이름의 쿠키가 있는지를 찾는 메소드
+    private Cookie findCookie(Cookie[] cookies, String cookieName){
+
+        Cookie targetCookie = null;
+
+        if(cookies!=null && cookies.length>0){
+            for(Cookie ck : cookies){
+                if(ck.getName().equals(cookieName)) {
+                    targetCookie = ck;
+                    break;
+                }
+            }
+        }
+
+        //만약 타겟 쿠키를 못찾으면 내가 새로 만들어 줌
+        if(targetCookie ==null){
+            targetCookie = new Cookie(cookieName,"");
+            targetCookie.setPath("/");
+            targetCookie.setMaxAge(60*60*24); //24시간 유지
+        }
+
+        return targetCookie;
+
     }
 }
