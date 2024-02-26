d79cc39bb6c64b98b2ab94141f8b228a956946f0
jher235 <tim668666@gmail.com>
2024-02-26 16:39:33 +0900

    타임리프 듀토리얼

---
+++ Java_Web_Workbook/b01/src/main/java/org/zerock/b01/domain/Board.java
@@ -0,0 +1,26 @@
+package org.zerock.b01.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tno;
+
+    @Column(length = 500, nullable = false)//칼럼의 길이, null허용 여부
+    private String title;
+
+    @Column(length = 2000, nullable = false)
+    private String content;
+
+    @Column(length = 50, nullable = false)
+    private String writer;
+}
