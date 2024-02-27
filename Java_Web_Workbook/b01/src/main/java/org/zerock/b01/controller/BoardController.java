cdfcc08f1756889898fb512dfa693460b0b38520
jher235 <tim668666@gmail.com>
2024-02-27 18:14:25 +0900

    등록 처리

--- Java_Web_Workbook/b01/src/main/java/org/zerock/b01/controller/BoardController.java
+++ Java_Web_Workbook/b01/src/main/java/org/zerock/b01/controller/BoardController.java
@@ -1,11 +1,16 @@
 package org.zerock.b01.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.b01.dto.BoardDTO;
 import org.zerock.b01.dto.PageRequestDTO;
 import org.zerock.b01.dto.PageResponseDTO;
 import org.zerock.b01.service.BoardService;
@@ -24,4 +29,45 @@ public class BoardController {
 
         model.addAttribute("responseDTO", responseDTO);
     }
+
+    @GetMapping("/register")
+    public void registerGet(){
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult,
+                               RedirectAttributes redirectAttributes){
+
+        log.info("board Post register..........");
+
+        if(bindingResult.hasErrors()){
+            log.info("has error");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+
+            return "redirect:/board/register";
+        }
+
+        log.info(boardDTO);
+
+        Long bno = boardService.register(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", bno);
+
+        return "redirect:/board/list";
+    }
+
+
+    @GetMapping("/read")
+    public void read(Long bno, PageRequestDTO pageRequestDTO, Model model){
+
+        BoardDTO boardDTO = boardService.readOne(bno);
+
+        log.info(boardDTO);
+
+        model.addAttribute("dto", boardDTO);
+
+    }
+
+
 }
