815cd2c292ebe8e09f950a372a2e8a76828e8ee2
jher235 <tim668666@gmail.com>
2024-02-29 12:05:48 +0900

    댓글 엔티티 생성 등록, 리스트업

---
+++ Java_Web_Workbook/b01/src/main/java/org/zerock/b01/domain/Reply.java
@@ -0,0 +1,28 @@
+package org.zerock.b01.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Reply", indexes = {
+        @Index(name = "idx_reply_board_bno", columnList = "board_bno")
+})
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = "board")    //toString을 할 때 참조하는 객체를 사용하지 않도록 exclude 속성값 지정
+public class Reply extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long rno;
+
+    @ManyToOne(fetch = FetchType.LAZY)  //연관 관계를 나타낼 때는 반드시 fetch속성은 LAZY로 지정
+    private Board board;
+
+    private String replyText;
+
+    private String replyer;
+
+}
---
+++ Java_Web_Workbook/b01/src/main/java/org/zerock/b01/reporsitory/ReplyRepository.java
@@ -0,0 +1,14 @@
+package org.zerock.b01.reporsitory;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.zerock.b01.domain.Reply;
+
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    //사용자 정의 JPQL 쿼리을 사용한다는 것. :bno는 파라미터로 전송되는 값.
+    @Query("select r from Reply r where r.board.bno = :bno")
+    Page<Reply> listOfBoard(Long bno, Pageable pageable);
+}
