db534d6acb0308c02fa151d37bf37c1f6531d95d
jher235 <tim668666@gmail.com>
2024-02-29 17:59:02 +0900

    service CRUD

---
+++ Java_Web_Workbook/b01/src/main/java/org/zerock/b01/service/ReplyServiceImpl.java
@@ -0,0 +1,60 @@
+package org.zerock.b01.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.zerock.b01.domain.Reply;
+import org.zerock.b01.dto.ReplyDTO;
+import org.zerock.b01.reporsitory.ReplyRepository;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class ReplyServiceImpl implements ReplyService{
+
+    private final ReplyRepository replyRepository;
+
+    private final ModelMapper modelMapper;
+
+    @Override
+    public Long register(ReplyDTO replyDTO) {
+        Reply reply = modelMapper.map(replyDTO, Reply.class);
+
+        Long rno = replyRepository.save(reply).getRno();
+
+        return rno;
+    }
+
+    @Override
+    public ReplyDTO read(Long rno) {
+
+        Optional<Reply> replyOptional = replyRepository.findById(rno);
+
+        Reply reply = replyOptional.orElseThrow();
+
+        return modelMapper.map(reply, ReplyDTO.class);
+    }
+
+    @Override
+    public void modify(ReplyDTO replyDTO) {
+
+        Optional<Reply> replyOptional = replyRepository.findById(replyDTO.getRno());
+
+        Reply reply = replyOptional.orElseThrow();
+
+        reply.changeText(replyDTO.getReplyText());
+
+        replyRepository.save(reply);
+
+    }
+
+    @Override
+    public void remove(Long rno) {
+
+        replyRepository.deleteById(rno);
+
+    }
+}
