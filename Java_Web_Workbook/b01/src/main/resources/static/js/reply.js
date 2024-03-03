080ea3f714ecaad0460098b7a7b158811b9d64a9
jher235 <tim668666@gmail.com>
2024-03-03 23:43:21 +0900

    댓글 구현

--- Java_Web_Workbook/b01/src/main/resources/static/js/reply.js
+++ Java_Web_Workbook/b01/src/main/resources/static/js/reply.js
@@ -5,3 +5,9 @@ async function get1(bno){
     return result.data
 }
 
+async function getList({bno, page, size, goLast}){
+    const result = await axios.get(`/replies/list/${bno}`, {params: {page, size}})
+
+    return result.data;
+}
+
