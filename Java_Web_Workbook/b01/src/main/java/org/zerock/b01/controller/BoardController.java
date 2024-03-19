3412cc3de553a75696008dbb663d76e12abeb435
jher235 <tim668666@gmail.com>
2024-03-19 09:58:18 +0900

    이미지 수정,  삭제, 게시글 삭제시 이미지 자동삭제

--- Java_Web_Workbook/b01/src/main/java/org/zerock/b01/controller/BoardController.java
+++ Java_Web_Workbook/b01/src/main/java/org/zerock/b01/controller/BoardController.java
@@ -3,6 +3,9 @@ package org.zerock.b01.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,11 +19,21 @@ import org.zerock.b01.dto.PageRequestDTO;
 import org.zerock.b01.dto.PageResponseDTO;
 import org.zerock.b01.service.BoardService;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
 @Controller
 @RequestMapping("/board")
 @Log4j2
 @RequiredArgsConstructor
 public class BoardController {
+
+    //실제 파일 삭제를 위해 경로 추가
+    @Value("${org.zerock.upload.path}")
+    private String uploadPath;
+
     private final BoardService boardService;
 
     @GetMapping("/list")
@@ -107,23 +120,73 @@ public class BoardController {
 
     }
 
+//    @PostMapping("/remove")
+//    public String remove(Long bno, PageRequestDTO pageRequestDTO, RedirectAttributes redirectAttributes){
+//
+//        log.info("remove post ......."+ bno);
+//
+//        String link = pageRequestDTO.getLink();
+//
+//        log.info(link);
+//
+//        log.info("--------"+pageRequestDTO);
+//
+//
+//        boardService.remove(bno);
+//
+//        redirectAttributes.addFlashAttribute("result", "removed");
+//
+//        return "redirect:/board/list?"+link;
+//    }
     @PostMapping("/remove")
-    public String remove(Long bno, PageRequestDTO pageRequestDTO, RedirectAttributes redirectAttributes){
+    public String remove(BoardDTO boardDTO, RedirectAttributes redirectAttributes){
+
+        Long bno = boardDTO.getBno();
+
         log.info("remove post ......."+ bno);
-
-        String link = pageRequestDTO.getLink();
-
-        log.info(link);
-
-        log.info("--------"+pageRequestDTO);
-
 
         boardService.remove(bno);
 
+        //게시물이 데이터베이스 상에서 삭제되었다면 첨부파일 삭제
+        log.info(boardDTO.getFileNames());
+        List<String> fileNames = boardDTO.getFileNames();
+
+        if(fileNames != null && fileNames.size()>0){
+            removeFiles(fileNames);
+        }
+
         redirectAttributes.addFlashAttribute("result", "removed");
 
-        return "redirect:/board/list?"+link;
+        return "redirect:/board/list";
+    }
 
+    public void removeFiles(List<String> files){
+
+        for (String fileName: files){
+
+//            Resource resource = new FileSystemResource(uploadPath+ File.separator+fileName);:
+//            파일 시스템 상의 경로(uploadPath)와 파일 이름(fileName)을 결합하여,
+//            해당 파일을 나타내는 Resource 객체를 생성합니다
+            Resource resource = new FileSystemResource(uploadPath+ File.separator+fileName);
+
+            String resourceName = resource.getFilename();
+
+            try{
+                String contentType = Files.probeContentType(resource.getFile().toPath());
+
+                resource.getFile().delete();
+
+                //섬네일이 존재한다면
+                if(contentType.startsWith("image")){
+                    File thumbnailFile = new File(uploadPath + File.separator + "s_"+fileName);
+
+                    thumbnailFile.delete();
+                }
+
+            } catch (IOException e) {
+                log.error(e.getMessage());
+            }
+        }
     }
 
 
