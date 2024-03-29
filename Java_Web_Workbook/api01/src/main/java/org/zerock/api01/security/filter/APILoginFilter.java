31d0e0adf3948159eaf339a1f04fa7968cf4cc6a
jher235 <tim668666@gmail.com>
2024-03-29 19:20:16 +0900

    인증 처리

--- Java_Web_Workbook/api01/src/main/java/org/zerock/api01/security/filter/APILoginFilter.java
+++ Java_Web_Workbook/api01/src/main/java/org/zerock/api01/security/filter/APILoginFilter.java
@@ -1,24 +1,30 @@
 package org.zerock.api01.security.filter;
 
 
+import com.google.gson.Gson;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Map;
 
 @Log4j2
 public class APILoginFilter extends AbstractAuthenticationProcessingFilter {
 
     //AbstractAuthenticationProcessingFilter생성자에 defaultFilterProcessUtl를 파라미터로 제공하며 생성
-    public APILoginFilter(String defaultFilterProcessUtl){
-        super(defaultFilterProcessUtl);
+    public APILoginFilter(String defaultFilterProcessUrl){
+        super(defaultFilterProcessUrl);
     }
 
+    //이 메소드는 인터페이스에서 상속 받은 메소드로, 요청이 들어올 때마다 호출
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request,
                                                 HttpServletResponse response)
@@ -26,8 +32,38 @@ public class APILoginFilter extends AbstractAuthenticationProcessingFilter {
 
         log.info("APILoginFilter -----------------------");
 
-        return null;
+        if(request.getMethod().equalsIgnoreCase("GET")){
+            log.info("GET METHOD NOT SUPPORT");
+            return null;
+        }
+
+        //GET방식이 아니라면 JSON 데이터를 파싱하여 사용자 아이디와 비밀번호를 포함한 맵 객체로 변환
+        Map<String, String> jsonData = parseRequestJSON(request);
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                jsonData.get("mid"),
+                jsonData.get("mpw"));
+
+        return getAuthenticationManager().authenticate(authenticationToken);
+
+//        log.info(jsonData);
+//        return null;
     }
 
+    private Map<String, String> parseRequestJSON(HttpServletRequest request){
+
+        //JSON 데이터를 분석해서 mid, mpw 전달값을 Map으로 처리
+        try(Reader reader = new InputStreamReader(request.getInputStream())){
+
+            Gson gson = new Gson();
+
+            return gson.fromJson(reader, Map.class);
+
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+
+        return null;
+    }
 
 }
