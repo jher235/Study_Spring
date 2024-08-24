0041421bdcf0f6052c17ac18a7602771ab8d6f34
jher235 <tim668666@gmail.com>
2024-08-24 22:57:47 +0900

    스프링 소켓 연습을 위한 프로젝트 생성

---
+++ stomp_websocket/src/main/java/com/example/stomp_websocket/StompWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.stomp_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StompWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StompWebsocketApplication.class, args);
+	}
+
+}
---
+++ stomp_websocket/src/test/java/com/example/stomp_websocket/StompWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stomp_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StompWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
