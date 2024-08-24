62dbaa1dac75d0e61f004a56237b20ac3da329a0
jher235 <tim668666@gmail.com>
2024-08-24 23:04:34 +0900

    helloMessage 모델 작성

---
+++ stomp_websocket/src/main/java/com/example/stomp_websocket/domain/HelloMessage.java
@@ -0,0 +1,14 @@
+package com.example.stomp_websocket.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class HelloMessage {
+    private String name;
+}
