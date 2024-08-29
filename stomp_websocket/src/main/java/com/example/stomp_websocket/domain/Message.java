ce7806a86cec2a0b356dbd203dab62753db4da2e
jher235 <tim668666@gmail.com>
2024-08-29 14:33:18 +0900

    주석 추가

--- stomp_websocket/src/main/java/com/example/stomp_websocket/domain/Message.java
+++ stomp_websocket/src/main/java/com/example/stomp_websocket/domain/Message.java
@@ -7,6 +7,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-public class Message {
+public class Message {  //수신자(혹은 수신 단체), 발신자도 작성해줄 수 있으면 좋음.
     String content;
 }
