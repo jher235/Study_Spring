dc74f44a820e31e19bad260707c1a3d7680bac8d
jher235 <tim668666@gmail.com>
2024-08-29 13:19:17 +0900

    test를 위한 어드민 컨트롤러 작성

---
+++ stomp_websocket/src/main/java/com/example/stomp_websocket/domain/Message.java
@@ -0,0 +1,12 @@
+package com.example.stomp_websocket.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Message {
+    String content;
+}
