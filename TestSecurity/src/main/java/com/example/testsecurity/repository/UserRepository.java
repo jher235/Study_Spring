6822be8d12d478403002e23b82840e225c819cb0
jher235 <tim668666@gmail.com>
2024-08-30 14:14:06 +0900

    커스텀 JPA 메서드 작성

--- TestSecurity/src/main/java/com/example/testsecurity/repository/UserRepository.java
+++ TestSecurity/src/main/java/com/example/testsecurity/repository/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     public boolean existsByUsername(String username);
+    public User findByUsername(String username);
 }
