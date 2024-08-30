eeb59ae7bff3a2090de0803b2f40bfdbb2b1d0d2
jher235 <tim668666@gmail.com>
2024-08-30 14:13:17 +0900

    패키지 구조 변경

--- TestSecurity/src/main/java/com/example/testsecurity/controller/AuthController.java
+++ TestSecurity/src/main/java/com/example/testsecurity/controller/AuthController.java
@@ -1,9 +1,8 @@
 package com.example.testsecurity.controller;
 
-import com.example.testsecurity.dto.JoinDto;
+import com.example.testsecurity.dto.request.JoinDto;
 import com.example.testsecurity.service.AuthService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
