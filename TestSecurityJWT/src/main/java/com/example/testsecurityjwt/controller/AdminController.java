bec55747cd2da10b8e4948b65617961b31645d50
jher235 <tim668666@gmail.com>
2024-09-19 23:58:00 +0900

    Spring Security + JWT 공부를 위한 프로젝트 생성

---
+++ TestSecurityJWT/src/main/java/com/example/testsecurityjwt/controller/AdminController.java
@@ -0,0 +1,9 @@
+package com.example.testsecurityjwt.controller;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ResponseBody
+public class AdminController {
+}
